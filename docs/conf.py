1167ad7237ce7abc394d4ce4832d2290bf36f4ae
Sam Nicholls <sam.n@studio8media.co.uk>
2016-06-14 19:04:12 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,9 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinxcontrib.napoleon',
 ]
+napoleon_google_docstring = False 
+napoleon_use_param = False
+napoleon_use_ivar = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

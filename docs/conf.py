7fa7d875b34123dd927b78578c25b6d2593149f8
Sam Nicholls <sam.n@studio8media.co.uk>
2016-06-15 00:24:38 +0100

    Sphinx 1.3 includes napoleon

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.intersphinx',
-    'sphinxcontrib.napoleon',
+    'sphinx.ext.napoleon',
 ]
 napoleon_google_docstring = False
 napoleon_use_param = False
@@ -120,7 +120,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
